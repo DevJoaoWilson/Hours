60877d7548368fe94b898b8d22b43c606339496f
João Wilson <jwmnascimento43@gmail.com>
2023-08-14 15:58:04 -0300

    Commit initial

---
+++ script.js
@@ -0,0 +1,19 @@
+function carregar(){
+    var img = document.getElementById('img')
+    var msg = document.getElementById('msg')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = (`Agora são exatamente ${hora} horas`)
+
+    if(hora > 0 && hora < 12){
+        img.src = "manha.png"
+        document.body.style.background = '#f9d487'
+    } else if(hora > 12 && hora < 18){
+        img.src = "tarde.png"
+        document.body.style.background = "#bf7d75"
+    }else{
+        img.src = "noite.png"
+        document.body.style.background = '#193d4b'
+    }
+}
